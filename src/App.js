fbed927546c66e4b0de26fd1b2b48b8b07d802e9
Evgenij Jakovlev <j238267431@mail.ru>
2021-08-06 15:55:15 +0500

    lesson2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect, useCallback, useMemo, useLayoutEffect, useReducer } from "react";
 
-function App() {
-  return (
+export const App = () =>  {
+    const [messageListRobot, setmessageListRobot] = useState([]);
+    const [messageList, setMessageList] = useState([]);
+    const [value, setValue] = useState('');
+
+    const sendMessageRobot = () => {
+        setmessageListRobot(state => [...state, {text: 'Hello From Robot', author: 'Robot'}]);
+    }
+    const sendMessage = () => {
+        setMessageList(state => [...state, {text: value, author: 'she'}]);
+        setValue("");
+        setAuthor({author: 'she'});
+    }
+    const [author, setAuthor] = useState('');
+    useEffect(()=>{
+        sendMessageRobot()
+    }, [author])
+    return (
     <div className="App">
-      <header className="App-header">
+        <input type="text" value={value} onChange={(e)=>setValue(e.target.value)}/>
+        <button onClick={sendMessage}>send message</button>
 
-      </header>
+        <h1>Messages:</h1>
+        <ul>
+        {messageList.map((message, id) => (
+            <li key={id}>
+                {message.author} {message.text}
+            </li>
+        ))}
+            {messageListRobot.map((message, id)=>(
+                <li>{message.author} {message.text} </li>
+            ))}
+
+        </ul>
     </div>
   );
 }
 
-export default App;
